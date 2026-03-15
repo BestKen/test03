786dd20e68eb5bdc63a0733a4ab1ad1d8ed1de83
Ken <ken@ltc.tw>
2026-03-15 20:17:31 +0800

    Initial Commit

---
+++ test03/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test03
+//
+//  Created by Ken Hsieh on 2026/3/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test03/test03App.swift
@@ -0,0 +1,17 @@
+//
+//  test03App.swift
+//  test03
+//
+//  Created by Ken Hsieh on 2026/3/15.
+//
+
+import SwiftUI
+
+@main
+struct test03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
